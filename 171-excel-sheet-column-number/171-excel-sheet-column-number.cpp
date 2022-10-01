a408bcd8b7b37d27d8bdf1e474459d6cd4460819
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-10-01 19:58:45 +0530

    Time: 3 ms (65.96%), Space: 5.9 MB (56.80%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    void fun(string c, int i, int &ans) {       // kittu ne kiya hai mere ;*;
+    int fun(string c, int i, int &ans) { //ye kitti ke kittu ne bataya hai behenchodddd
         if(i == c.size())
-            return;
+            return ans;
         ans = ans*26*1L + c[i]-64;
-        fun(c, i+1, ans);
+        return fun(c, i+1, ans);
     }
     int titleToNumber(string c) {
         int ans = 0;
-        fun(c, 0, ans);
-        return ans;
+        return fun(c, 0, ans);
     }
 };
         // for(int i=0; i<n; ++i)
