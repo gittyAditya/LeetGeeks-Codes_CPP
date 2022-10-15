eabe4fa66b07df1df898e989e27fb4951a7ac15e
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-10-15 23:59:19 +0530

    Time: 15 ms (31.63%), Space: 7.3 MB (77.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string t = "";
-        int n = s.size();
-        for(int i=0; i<n; ++i){
-            if(s[i] > 64 && s[i] < 91)
-                t += tolower(s[i]);
-            else if(s[i] > 96 && s[i] < 123 || s[i] > 47 && s[i] < 58)
-                t += s[i];
-        }
-        int i=0, j=t.size()-1;
-        while(i<j)
-            if(t[i++] != t[j--])
+        int start = 0, end = s.size()-1;
+        while(start<end){
+            while(start < end && !isalnum(s[start]))
+                ++start;
+            while(start < end && !isalnum(s[end]))
+                --end;
+            if(tolower(s[start++]) != tolower(s[end--]))
                 return false;
+        }
         return true;
     }
 };
