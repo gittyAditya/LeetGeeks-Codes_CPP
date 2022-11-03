7487d6aa7b5fcf461e2c3d440104d40812fd88af
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-11-04 01:33:22 +0530

    Time: 4 ms (43.47%), Space: 6.3 MB (56.69%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-        int ans = 0;
-        int count = 0;
-        int n = s.size();
+    int minAddToMakeValid(string s) {   // Divya kitni cool hai!!! (aww, unuhu, ahhahaha, hehey, arreeh, aanaanashdasdasodchuphoja, ahaha uuuuu, ahaanahannnaaa, unnnnnn, Adityaaaaaa, Aditya yaar unhunhunhun, ahaaahaha, ughhahusha, auuuuu, aaaaah, unhunhun, gaal dukh rhe hai chup hoja unnnnn, uhhahaha, hushsuhaananna, xxxxxx, Aditya unnnzuzuzuudududuuddu, Aditya chup heeeeehehehheheh, ah, submit kar de)
+        int 
+            ans = 0,
+            count = 0,
+            n = s.size();
+        
         for(int i=0; i<n; ++i){
             if(count > 0 && s[i] == ')')
                 --count;
@@ -11,6 +13,6 @@ public:
                 ++ans;
             else ++count;
         }
-        return ans+count;
+        return count+ans;
     }
 };
