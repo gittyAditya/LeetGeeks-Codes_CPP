319b56d7d30391020bab58e0c483978f35594db9
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-11-09 02:53:50 +0530

    Time: 7 ms (40.76%), Space: 7 MB (22.46%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -5,11 +5,9 @@ public:
         int n = s.size();
         stack <char> stk;
         for(int i=0; i<n; ++i){
-            if(!stk.empty() && abs(stk.top()-s[i]) == 32){
+            if(!stk.empty() && abs(stk.top()-s[i]) == 32)
                 stk.pop();
-                continue;
-            }
-            stk.push(s[i]);
+            else stk.push(s[i]);
         }
         while(!stk.empty()){
             ans = stk.top() + ans;
