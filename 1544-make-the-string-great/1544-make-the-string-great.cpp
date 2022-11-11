789eaad0821746c3d5690c30e4be9081ffa796bf
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-11-12 04:32:36 +0530

    Time: 7 ms (37.90%), Space: 6.3 MB (49.85%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -4,11 +4,9 @@ public:
         string ans;
         int n = s.size();
         for(int i=0; i<n; ++i){
-            ans.push_back(s[i]);
-            while(ans.size() && (ans.back()==s[i+1]+32 || ans.back()==s[i+1]-32)){
+            if(ans.size() && (ans.back()==s[i]+32 || ans.back()==s[i]-32))
                 ans.pop_back();
-                ++i;  // get one extra ahead because we've been checking 'i+1'
-            }
+            else ans.push_back(s[i]);
         }
         return ans;
     }
