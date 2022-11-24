2e00598b47128d74a467f13d4cadd99b111c0883
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-11-24 06:46:23 +0530

    Time: 64 ms (17.35%), Space: 20.1 MB (37.30%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,21 @@
-class Solution      // ah shit, here we go again, for the sake of submission...
-{
+class Solution {
 public:
-    bool isValidSudoku(vector<vector<char> > &board)
-    {
-        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
-        
-        for(int i = 0; i < board.size(); ++ i)
-            for(int j = 0; j < board[i].size(); ++ j)
-                if(board[i][j] != '.')
-                {
-                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                    if(used1[i][num] || used2[j][num] || used3[k][num])
-                        return false;
-                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
-                }
-        
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        for(int i=0; i<9; ++i){
+            for(int j=0; j<9; ++j){
+                if(board[i][j] == '.')
+                    continue;
+                string r = to_string(i) + 'r' + board[i][j];
+                string c = to_string(j) + 'c' + board[i][j];
+                string b = to_string(i/3) + to_string(j/3) + 'b' + board[i][j];
+                if(st.find(r) != st.end() || st.find(c) != st.end() || st.find(b) != st.end())
+                    return false;
+                st.insert(r);
+                st.insert(c);
+                st.insert(b);
+            }
+        }
         return true;
     }
 };
