957fb707e3358f52bdc4beb4dfbed81b3c282bc0
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-12-01 03:38:01 +0530

    Time: 6 ms (54.71%), Space: 8 MB (86.29%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        for(auto i:arr)
+            mp[i]++;
+        unordered_set<int> st;
+        for(auto i:mp)
+            st.insert(i.second);
+        return mp.size() == st.size() ? true : false;
+    }
+};
