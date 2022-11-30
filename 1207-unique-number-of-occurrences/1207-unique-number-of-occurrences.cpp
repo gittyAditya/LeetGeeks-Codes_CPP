a6c02381af3061b864dc6f0ed9b73954b830fa82
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-12-01 04:30:04 +0530

    Time: 12 ms (15.32%), Space: 8.7 MB (6.58%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -4,10 +4,11 @@ public:
         vector<int> freq(2001);     // values in range => counting sort 0_o
         for(auto i:arr)
             freq[i+1000]++;
-        sort(freq.begin(), freq.end());
-        for(int i=0; i<2000; ++i)
-            if(freq[i] && freq[i] == freq[i+1])
+        sort(freq.begin(), freq.end(), greater<int>());
+        for(int i=0; i<2000; ++i){
+            if(freq[i] && freq[i] == freq[i+1])     // avoid the zeros
                 return false;
+        }
         return true;
     }
 };
