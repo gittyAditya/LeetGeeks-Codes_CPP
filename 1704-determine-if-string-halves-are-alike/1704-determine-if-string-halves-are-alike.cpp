8bfaaebce3b997e5a54bc612d021dda8b2108fc3
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-12-01 15:26:52 +0530

    Time: 7 ms (48.94%), Space: 6.8 MB (16.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    bool halvesAreAlike(string s) {
+    bool halvesAreAlike(string s) {         // constant space, clean code, no hardcodes;
         int n = s.size();
         int c = 0;
+        unordered_set<char> st = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         for(int i=0; i<n/2; ++i)
-            if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            if(st.count(s[i]))
                 ++c;
         for(int i=n/2; i<n; ++i)
-            if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            if(st.count(s[i]))
                 --c;
         return c==0 ? true : false;
     }
