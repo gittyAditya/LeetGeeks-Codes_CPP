8ecd531bb0054424ccd37da2e1fab2e9f0fd9b25
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2022-12-22 23:58:39 +0530

    Time: 4 ms (23.39%), Space: 6 MB (33.74%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -3,6 +3,6 @@ public:
     int numberOfSteps(int num) {
         if(num == 0)
             return 0;
-        return 1 + (num%2 ? numberOfSteps(num-1) : numberOfSteps(num/2));
+        return 1 + numberOfSteps(num%2 ? num-1 : num/2);
     }
 };
