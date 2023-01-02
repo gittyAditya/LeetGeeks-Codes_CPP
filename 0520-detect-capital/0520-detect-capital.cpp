c65d6dcb3ed90ce52a2849f6ce6beaf5c455a420
Aditya Gautam <90149923+gittyAditya@users.noreply.github.com>
2023-01-02 14:33:16 +0530

    Time: 5 ms (24.37%), Space: 6.1 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length();
+        if(word[0] >= 'A' && word[0] <= 'Z'){
+            int count = 1;
+            for(int i=1; i<n; ++i){
+                if(word[i] >= 'A' && word[i] <= 'Z')
+                    count++;
+            }
+            return (count == 1 || count == n);
+        }
+        else {
+            for(int i=1; i<n; ++i){
+                if(word[i] < 'a')
+                    return false;
+            }
+        }
+        return true;
+    }
+};
